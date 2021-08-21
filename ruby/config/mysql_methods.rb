737bbb7b2fc1bfee35ceb445ff0f015ff038981c
sue445 <sue445@sue445.net>
2021-08-21 10:14:30 +0900

    Add snippets

---
+++ ruby/config/mysql_methods.rb
@@ -0,0 +1,58 @@
+require 'securerandom'
+require_relative "./sentry_methods"
+
+module MysqlMethods
+  include SentryMethods
+
+  # mysqlコマンドを実行して結果を一時ファイルに出力する。ブロック内から抜けると一時ファイルは削除される
+  # @param sql [String]
+  # @param quote [Boolean] csvのセルをダブルクオーテーションで囲むかどうか
+  # @param suffix_sql [String] クエリの末尾につける文字列
+  # @yieldparam 一時ファイルのパス
+  def with_create_csv_file_from_mysql(sql, quote: true, suffix_sql: nil)
+    # TODO: mysqlやmariadbのserviceファイルでPrivateTmpを無効化しないと/tmpにファイルが出力できない
+    dist_file = File.join("/tmp", "#{SecureRandom.uuid}.csv")
+
+    if defined?(::NRMysql2Client)
+      NRMysql2Client.with_newrelic(sql) do
+        create_csv_file_from_mysql(sql, dist_file: dist_file, quote: quote, suffix_sql: suffix_sql)
+      end
+    else
+      create_csv_file_from_mysql(sql, dist_file: dist_file, quote: quote, suffix_sql: suffix_sql)
+    end
+
+    # DBサーバに出力されているのでscpでローカルに転送する
+    # TODO: scpするにはdbサーバのauthorized_keysにAPの公開鍵が登録されている必要がある
+    unless ENV["DB_HOST"] == "127.0.0.1"
+      system_with_sentry("scp -i /home/isucon/.ssh/id_ed25519 isucon@#{ENV["DB_HOST"]}:#{dist_file} #{dist_file}")
+    end
+
+    yield dist_file
+  ensure
+    FileUtils.rm_f(dist_file)
+  end
+
+  # mysqlコマンドを実行して結果をCSVファイルに出力する
+  # @param sql [String]
+  # @param dist_file [String] DBサーバの出力先のパス
+  # @param quote [Boolean] csvのセルをダブルクオーテーションで囲むかどうか
+  # @param suffix_sql [String] クエリの末尾につける文字列
+  def create_csv_file_from_mysql(sql, dist_file:, quote: true, suffix_sql: nil)
+    into_outfile_sql = %Q("#{sql} INTO OUTFILE '#{dist_file}' FIELDS TERMINATED BY ',')
+    into_outfile_sql << %Q( OPTIONALLY ENCLOSED BY '\\"') if quote
+    into_outfile_sql << " " + suffix_sql if suffix_sql
+    into_outfile_sql << '"'
+
+    command = [
+      "mysql",
+      "-u", ENV["DB_USER"],
+      "-p#{ENV["DB_PASS"]}",
+      "-h", ENV["DB_HOST"],
+      ENV["DB_DATABASE"],
+      "-e",
+      into_outfile_sql,
+    ].join(" ")
+
+    system_with_sentry(command)
+  end
+end
---
+++ ruby/config/redis_methods.rb
@@ -0,0 +1,54 @@
+require "redis"
+require "connection_pool"
+require "oj"
+
+$redis = ConnectionPool::Wrapper.new(size: 32, timeout: 3) { Redis.new(host: ENV["REDIS_HOST"]) }
+
+::Oj.default_options = { mode: :compat }
+
+module RedisMethods
+  # redisにあればredisから取得し、キャッシュになければブロック内の処理で取得しredisに保存するメソッド（Rails.cache.fetchと同様のメソッド）
+  #
+  # @param cache_key [String]
+  # @param enabled [Boolean] キャッシュを有効にするかどうか
+  # @param is_object [Boolean] String以外を保存するかどうか
+  #
+  # @yield キャッシュがなかった場合に実データを取得しにいくための処理
+  # @yieldreturn [Object] redisに保存されるデータ
+  #
+  # @return [Object] redisに保存されるデータ
+  def with_redis(cache_key, enabled: true, is_object: false)
+    unless enabled
+      return yield
+    end
+
+    cached_response = $redis.get(cache_key)
+    if cached_response
+      if is_object
+        # return Marshal.load(cached_response)
+        return Oj.load(cached_response)
+      else
+        return cached_response
+      end
+    end
+
+    actual = yield
+
+    if actual
+      if is_object
+        # data = Marshal.dump(actual)
+        data = Oj.dump(actual)
+
+        $redis.set(cache_key, data)
+      else
+        $redis.set(cache_key, actual)
+      end
+    end
+
+    actual
+  end
+
+  def initialize_redis
+    $redis.flushall
+  end
+end
